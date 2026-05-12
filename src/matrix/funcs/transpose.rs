42a955c5bf37bdfd4195f1f314edaeb2154ecd83
Abdullah ibrahim Durusoy <adurusoy@k2m09s01.42kocaeli.com.tr>
2026-05-12 16:06:42 +0300

    Changed transpose to referance only

--- src/matrix/funcs/transpose.rs
+++ src/matrix/funcs/transpose.rs
@@ -2,7 +2,7 @@ use crate::matrix::Matrix;
 use num_traits::Float;
 
 impl<K: Float> Matrix<K> {
-    pub fn transpose(&mut self) -> Matrix<K> {
+    pub fn transpose(&self) -> Matrix<K> {
         let mut new_vec = vec![K::zero(); self.data.len()];
         for i in 0..self.rows {
             for j in 0..self.cols {
