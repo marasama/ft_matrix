90f5bd649c3814f3e7ccf2e9bc62cc5fa0e82c0e
Zart Zort <abdullah.durusoy31@gmail.com>
2026-06-15 19:46:05 +0300

    Adding from each row and column added for matrix

--- src/matrix/funcs/each.rs
+++ src/matrix/funcs/each.rs
@@ -1,10 +1,10 @@
-use std::ops::SubAssign;
+use std::ops::{AddAssign, SubAssign};
 
 use crate::matrix::Matrix;
 use crate::vector::Vector;
 use num_traits::Float;
 
-impl<K: Float + SubAssign> Matrix<K> {
+impl<K: Float + SubAssign + AddAssign> Matrix<K> {
     pub fn sub_each_col(&self, v: &Vector<K>) -> Matrix<K> {
         assert_eq!(
             self.rows,
@@ -29,6 +29,43 @@ impl<K: Float + SubAssign> Matrix<K> {
             self.cols,
             v.size(),
             "Columns sizes must be equal to substract the vector from the each row of the matrix!"
+        );
+        let mut new_data = self.data.clone();
+        for r in 0..self.rows {
+            for c in 0..self.cols {
+                new_data[r * self.cols + c] -= v.data[c];
+            }
+        }
+        Matrix {
+            data: new_data,
+            rows: self.rows,
+            cols: self.cols,
+        }
+    }
+    pub fn add_each_col(&self, v: &Vector<K>) -> Matrix<K> {
+        assert_eq!(
+            self.rows,
+            v.size(),
+            "Row sizes must be equal to add the vector to the each column of the matrix!"
+        );
+        let mut new_data = self.data.clone();
+        for r in 0..self.rows {
+            let val = v.data[r];
+            for c in 0..self.cols {
+                new_data[r * self.cols + c] += val;
+            }
+        }
+        Matrix {
+            data: new_data,
+            rows: self.rows,
+            cols: self.cols,
+        }
+    }
+    pub fn add_each_row(&self, v: &Vector<K>) -> Matrix<K> {
+        assert_eq!(
+            self.cols,
+            v.size(),
+            "Columns sizes must be equal to add the vector to the each row of the matrix!"
         );
         let mut new_data = self.data.clone();
         for r in 0..self.rows {
